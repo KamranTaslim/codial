bb3fbb6500b7b81bab48cddd61a40c9d27595911
Mohammad Kamran Taslim <kamrantaslimdos@gmail.com>
2023-10-09 22:18:30 +0530

    Add Delete post function in controller

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 async function createPost(req, res) {
   try {
@@ -13,3 +14,39 @@ async function createPost(req, res) {
 }
 
 module.exports.create = createPost;
+
+// module.exports.destroy = function (req, res) {
+//   Post.findById(req.params.id, function (err, post) {
+//     //.id means converting the object id into String
+
+//     if (post.user == req.user.id) {
+//       post.remove();
+
+//       Comment.deleteMany({ post: req.params.id }, function (err) {
+//         return res.redirect("back");
+//       });
+//     } else {
+//       return res.redirect("back");
+//     }
+//   });
+// };
+
+async function destroy(req, res) {
+  try {
+    const post = await Post.findById(req.params.id).exec();
+
+    if (post.user.toString() === req.user.id.toString()) {
+      await post.deleteOne();
+
+      await Comment.deleteMany({ post: req.params.id }).exec();
+      return res.redirect("back");
+    } else {
+      return res.redirect("back");
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Something went wrong." });
+  }
+}
+
+module.exports.destroy = destroy;
